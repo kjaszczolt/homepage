7f9b6b58285dc55bb0fb337834cfa5e27cd8159a
Kamila Jaszczolt <camilla19.944@gmail.com>
2025-01-16 18:37:39 +0100

    icon & OpenGraph image added

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,30 @@
-console.log("Hejka!");
-
-let buttonItem = document.querySelector(".buttonItem");
-let photoItem = document.querySelector(".photoItem");
-let button = document.querySelector(".button");
-let changeButton = document.querySelector(".changeButton");
-
-buttonItem.addEventListener("click", () => {
-    photoItem.remove();
-
-    if (photoItem.remove) {
-        changeButton.innerText = "Zdjęcie usunięte";
-    } else {
-        changeButton.innerText = "Usuń zdjęcie"
+{
+    const welcome = () => {
+        console.log("Hejka! Pozdrawiam wszystkich odwiedzających developerów!");
     }
-});
+
+    welcome();
+}
+
+const button = document.querySelector(".button");
+
+{
+    const removePhotoItemClick = () => {
+        const buttonItem = document.querySelector(".buttonItem");
+        const photoItem = document.querySelector(".photoItem");
+        const changeButton = document.querySelector(".changeButton");
+
+        buttonItem.addEventListener("click", () => {
+            photoItem.remove();
+
+            if (photoItem.remove) {
+                changeButton.innerText = "Zdjęcie usunięte";
+            } else {
+                changeButton.innerText = "Usuń zdjęcie"
+            }
+        });
+    }
+
+    removePhotoItemClick();
+
+}
